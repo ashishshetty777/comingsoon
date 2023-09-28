c94c2cafd888dc8c4d5e5f9fe0c03cffd59b76bc
Ashish Shetty <>
2023-09-28 16:43:54 +0530

    seo desc changed

--- src/data/settings.ts
+++ src/data/settings.ts
@@ -2,5 +2,5 @@ export const settings = {
   site: 'https://lucid3.io',
   name: 'Lucid Bags',
   title: 'Lucid Bags | Luxury Tech Apparel Brand',
-  description: 'Lucid 3 where tech andElevate your style with the fusion of luxury and cutting-edge technology from Lucid 3',
+  description: 'Elevate your style with the fusion of luxury and cutting-edge technology from Lucid 3',
 }
